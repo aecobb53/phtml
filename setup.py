e5f881dc75dcec2ba6a7b6d1fa835a2295ce201d
Andrew Cobb <aecobb53@gmail.com>
2022-12-25 17:07:35 -0700

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='phtml',
-    version='0.4.2',
+    version='0.4.3',
     author='Andrew Cobb',
     author_email='aecobb53@gmail.com',
     description='Manages an html file in python form',
